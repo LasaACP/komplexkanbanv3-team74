8b427ccfbd1b5f8096f25abe38617d9e7dd05a07
TheUnrealOculus <ved@matai.in>
2023-12-05 19:21:10 +0000

    fix push

---
+++ src/complex.cpp
@@ -0,0 +1,15 @@
+class complex{
+
+  private:
+    double real;
+    double imaginary;
+
+  public:
+    complex(){real = 0; imaginary = 0;}
+    complex(double r, double i = 0) {real = r; imaginary = i;}
+
+
+  complex operator* (const complex& c){
+    
+  };
+};
