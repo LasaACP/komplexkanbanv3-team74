3fa40bd44b73f9e74417bb885379f05ee397dfe6
AdityaGhatty <aditya.ghattyyc@stu.austinisd.org>
2023-12-05 20:05:23 +0000

    add, subtract, == operators

--- src/complex.cpp
+++ src/complex.cpp
@@ -12,4 +12,25 @@ class complex{
   complex operator* (const complex& c){
 
   }
+
+  complex operator+(complex a){
+  double c = a.real + real;
+  double d = a.imaginary + imaginary;
+  complex* e = new complex(c, d);
+  return *e;
+  }
+
+  complex operator-(complex a){
+    double b = real - a.real;
+    double c = imaginary - a.imaginary;
+    complex *e = new complex(b, c);
+    return *e;
+  }
+
+  complex operator==(complex a){
+    return(a.real == real && a.imaginary == imaginary);
+  }
+
+  
+
 };
