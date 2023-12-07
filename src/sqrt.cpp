7bd7c946de6b00defe917122f24fbdebaa9bbad5
theunrealoculus <ved@matai.in>
2023-12-07 19:06:01 +0000

    Naming Changes

---
+++ src/sqrt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include "sqrt.h"
+#include "complex.h"
+
+/*complex sqrt(complex c) {
+     complex resulted=pow(c,0.5);
+    return resulted;
+  }*/
+complex sqrt(complex c) {
+  return c;
+}
