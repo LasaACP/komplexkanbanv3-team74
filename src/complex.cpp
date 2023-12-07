1b471bd9f8aaf122df772083fd29577144cc363e
AdityaGhatty <aditya.ghattyyc@stu.austinisd.org>
2023-12-07 18:52:24 +0000

    operator functions

--- src/complex.cpp
+++ src/complex.cpp
@@ -1,36 +1,42 @@
-class complex{
+class complex {
 
-  private:
-    double real;
-    double imaginary;
+private:
+  double real;
+  double imaginary;
 
-  public:
-    complex(){real = 0; imaginary = 0;}
-    complex(double r, double i = 0) {real = r; imaginary = i;}
-
-
-  complex operator* (const complex& c){
-
+public:
+  complex() {
+    real = 0;
+    imaginary = 0;
+  }
+  complex(double r, double i = 0) {
+    real = r;
+    imaginary = i;
   }
 
-  complex operator+(complex a){
-  double c = a.real + real;
-  double d = a.imaginary + imaginary;
-  complex* e = new complex(c, d);
-  return *e;
-  }
+  complex operator*(const complex &c) {}
 
-  complex operator-(complex a){
+  complex operator+(complex a) {
+    double c = a.real + real;
+    double d = a.imaginary + imaginary;
+    complex *e = new complex(c, d);
+    return *e;
+  
+  complex operator-(complex a) {
     double b = real - a.real;
     double c = imaginary - a.imaginary;
     complex *e = new complex(b, c);
     return *e;
   }
 
-  complex operator==(complex a){
-    return(a.real == real && a.imaginary == imaginary);
+  complex operator==(complex a) {
+    return (a.real == real && a.imaginary == imaginary);
   }
 
+  complex operator =(complex a){
+    real = a.real;
+    imaginary = a.imaginary;
+    return *this;
+  }
   
-
 };
