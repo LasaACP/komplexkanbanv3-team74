9dbed0204f484a413ded57f7851fc8615e2deb70
jilugom <hellojustinkwon@gmail.com>
2023-12-12 07:03:01 +0000

    started acos func.

--- src/acos/acos.cpp
+++ src/acos/acos.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
-#include <cmath>
 #include "acos.h"
 #include "../complex.h"
+#include "../pow.cpp"
+#include "pow.h"
+#include <cmath>
+#include <iostream>
 
 complex acos(const complex c) {
-  return c;
+  complex i = complex(0, 1);
+
+  return -i * log(c + i * sqrt(1.0 - pow(c,2)));
 }
