363ed7dcbe5042bbfff5c15f4c8e8c640b18fe61
justinwickelgren <justin.wickelgren@outlook.com>
2023-12-12 21:16:32 +0000

    added pow

--- src/inserter/inserter.cpp
+++ src/inserter/inserter.cpp
@@ -2,6 +2,9 @@
 #include "../complex.h"
 
 ostream& operator<<(ostream& out, const complex& c) {
-  out << c.real << "+" << c.imaginary << "i";
+  if (c.imaginary >= 0) {
+    out << c.real << "+" << c.imaginary << "i";
+  } else if (c.imaginary < 0) {
+    out << c.real << "-" << -c.imaginary << "i";
   return out;
 }
--- src/pow/pow.cpp
+++ src/pow/pow.cpp
@@ -1,8 +1,11 @@
 #include "pow.h"
 #include "../complex.h"
+#include "../arg/arg.h"
+#include <cmath>
+#include <bits/stdc++.h>
 
 complex pow(double b, const complex exp) {
-  return exp;
+  return complex(cos(exp.imaginary*log(b)), sin(exp.imaginary)*log(b)) * pow(b, exp.real);
 }
 
 complex pow(const complex b, int exp) {
@@ -14,9 +17,12 @@ complex pow(const complex b, int exp) {
 }
 
 complex pow(const complex b, double exp) {
-  return b;
+  double r = sqrt(b.real*b.real + b.imaginary*b.imaginary);
+  double theta = atan(b.imaginary/b.real);
+  return complex(cos(exp*theta), sin(exp*theta)) * pow(r, exp);
 }
 
-complex pow(const complex b, const complex exp) {
-  return exp;
+complex pow(const complex b, const complex exp1) {
+  double r = sqrt(b.real*b.real + b.imaginary*b.imaginary);
+  return pow(2.718281828459045, (complex(exp1.real, exp1.imaginary) * log(r)) + (complex(0,1) * exp1 * arg(b))) * pow(r, exp1);
 }
