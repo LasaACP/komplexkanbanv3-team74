8ad1b161fd6b11f62a8ba26b093e71921cd68b32
theunrealoculus <ved@matai.in>
2023-12-13 05:16:52 +0000

    tan func

--- src/tan/tan.cpp
+++ src/tan/tan.cpp
@@ -1,6 +1,11 @@
 #include "../complex.h"
+#include <cmath>
 #include "tan.h"
 
 complex tan(const complex c) {
-  return c;
+  double real = c.real;
+  double imag = c.imaginary;
+  double r = (tan(real)-(tan(real)*tanh(imag)*tanh(imag)))/(1+(tan(real)*tan(real)*tanh(imag)*tanh(imag)));
+  double i = (tanh(imag)+(tan(real)*tan(real)*tanh(imag)))/(1+(tan(real)*tan(real)*tanh(imag)*tanh(imag)));
+  return complex(r, i);
 }
